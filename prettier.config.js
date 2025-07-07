c43a6418813e2f9d8ba3b4ea05d980c6f0b57914
victor-assuncao <victorassuncao17@gmail.com>
2025-07-07 07:53:37 -0300

    adiciona o `.prettierignore`

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/** @type {import("prettier").Config} */
+const config = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    printWidth: 150,
+};
+
+module.exports = config;
